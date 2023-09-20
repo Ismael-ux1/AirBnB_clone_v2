cd2590b8b1bfe631aaec21d6b70589cf6477ed8e
BABABUSOLA <queenbusola1@gmail.com>
2023-09-20 13:46:46 -0700

    feat state creation and get states

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import Column, Integer, String, DateTime
 import uuid
 from datetime import datetime
 
@@ -53,7 +53,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary.keys():
-            del my_dict['_sa_instance_state']
+            del dictionary['_sa_instance_state']
         return dictionary
 
     def delete(self):
