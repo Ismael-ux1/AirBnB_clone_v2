3e00b6ff26198edd5c313c71b68fbc73514a96cf
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2023-10-22 10:06:17 -0700

    a script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,10 @@ from flask import Flask
 # Create a Flask application instance
 app = Flask(__name__)
 
+
 # Define a route for the root URL ('/') and allow trailing slashes
 @app.route('/', strict_slashes=False)
+# Function to return “Hello HBNB!” at root URL
 def hello_hbnb():
     return 'Hello HBNB!'
 
